8320169a429f924d13781cc3b21f473ed1c60282
Shadrin Igor <igor@shadrins.ru>
2025-07-03 15:35:18 +0300

    Eng docstrings

--- topico_mapi.py
+++ topico_mapi.py
@@ -5,34 +5,34 @@ from pydantic import BaseModel, Field
 
 
 class UserRole(BaseModel):
-    """Ролевая модель"""
+    """Role model"""
     id: UUID
     name: str
     description: str
 
 class User(BaseModel):
-    """Модель пользователя"""
+    """User model"""
     topico_id: UUID = Field(description="Internal topico team user ID")
     minecraft_id: UUID
     telegram_id: int
     role_id: UUID = Field(description="UserRole id")
 
 class Ticket(BaseModel):
-    """Модель заявки"""
+    """Ticket model"""
     id: UUID
     topic_id: int = Field(description="Telegram topic ID")
     user_id: int = Field(description="Telegram user ID")
     dt: datetime
 
 class TicketMessage(BaseModel):
-    """Модель сообщения в заявке"""
+    """Ticket message model"""
     ticket_id: UUID
     user_id: int = Field(description="Telegram user ID")
     message: str
     dt: datetime
 
 class TicketAction(BaseModel):
-    """Модель действия в заявке (Пример: Закрытие)"""
+    """Ticket action model. Example: Ticket closing"""
     ticket_id: UUID
     user_id: int = Field(description="Telegram user ID")
     action: str
