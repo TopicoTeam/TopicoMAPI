fd092fdc1c9f75840c8140d12d36558857cc7465
Shadrin Igor <igor@shadrins.ru>
2025-07-05 13:19:20 +0300

    New routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.user import get_user_by_telegram, register
+from src.topico_api_client.user import get_user_by_telegram
 
 print(get_user_by_telegram(id=307345124))
 # print(register(telegram_id=307345121))
